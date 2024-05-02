b6f2ae60ed07567dbb5d7e7f270dc0aca46850c8
Marlon McPherson <583056+mcpherson@users.noreply.github.com>
2024-05-02 22:00:03 +0000

    Added basic user/password authentication

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const fs = require("fs");
+const USER = process.env.USER;
+const PASSWORD = process.env.PASSWORD;
 
 const app = express();
 const port = 3000;
@@ -8,12 +10,22 @@ app.use(express.json());
 app.use(express.text());
 
 app.post("/", (req, res) => {
-  console.log(req.body);
+  let auth = Buffer.from(req.headers.authorization.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+
+  let usr = auth[0];
+  let pw = auth[1];
+
+  if (usr !== USER || pw !== PASSWORD) {
+    res.send("Nice try.");
+  }
+
   fs.appendFile("test.txt", req.body + "\n", (err) => {
     if (err) throw err;
-    console.log("Text appended to file!");
+    console.log("Text appended to file!" + USER + PASSWORD);
   });
-  res.send("Hello World!");
+  res.send();
 });
 
 app.get("/", (req, res) => {
