42ec42ccd60a5c859d2057c16fb0d73dccfa64b7
Marlon McPherson <583056+mcpherson@users.noreply.github.com>
2024-05-03 00:21:21 +0000

    Matched paths to nginx config

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const fs = require("fs");
-const USER = process.env.USER;
+const USER = process.env.USR;
 const PASSWORD = process.env.PASSWORD;
 
 const app = express();
@@ -9,31 +9,31 @@ const port = 3000;
 app.use(express.json());
 app.use(express.text());
 
-app.post("/", (req, res) => {
+app.post("/mcpherson", (req, res) => {
   let auth = Buffer.from(req.headers.authorization.split(" ")[1], "base64")
     .toString()
     .split(":");
 
   let usr = auth[0];
   let pw = auth[1];
-
   if (usr !== USER || pw !== PASSWORD) {
     res.send("Nice try.");
+    return;
   }
 
   fs.appendFile("test.txt", req.body + "\n", (err) => {
     if (err) throw err;
-    console.log("Text appended to file!" + USER + PASSWORD);
   });
   res.send();
 });
 
-app.get("/", (req, res) => {
+app.get("/mcpherson", (req, res) => {
   fs.readFile("test.txt", "utf8", (err, data) => {
     if (err) {
       console.error(err);
       return;
     }
+    res.set("content-type", "text/plain");  
     res.send(data);
   });
 });
